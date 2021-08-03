649e3564b1bc6088332eaa17035a0db42a29924b
Fabricio <fabricioeliasdesouza@gmail.com>
2021-08-02 23:44:00 -0300

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( style.scss components.scss stisla.js custom.js custom.scss
                                                   icon_new_only.png icon_new_with_name.png avatar-1.jpg
-                                                  trophy_gold.png trophy_silver.png trophy_bronze.png    )
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+                                                  trophy_gold.png trophy_silver.png trophy_bronze.png )
+Rails.application.config.assets.precompile += %w( fa-brands-400.eot fa-brands-400.svg fa-brands-400.ttf fa-brands-400.woff
+                                                  fa-brands-400.woff2 fa-regular-400.eot fa-regular-400.svg fa-regular-400.ttf
+                                                  fa-regular-400.woff fa-regular-400.woff2 fa-solid-900.eot fa-solid-900.svg
+                                                  fa-solid-900.ttf fa-solid-900.woff fa-solid-900.woff2 )
+Rails.application.config.assets.precompile += /\.(?:svg|eot|woff|ttf)$/
 Rails.application.config.assets.paths << Rails.root.join("lib")
 Rails.application.config.assets.paths << Rails.root.join("vendor")
 
