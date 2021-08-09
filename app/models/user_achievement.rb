5bb326c4b91f643caab646da0897ada0356c286d
Fabricio <fabricioeliasdesouza@gmail.com>
2021-08-09 19:08:43 -0300

    resolvido problema de relacionamento user

--- app/models/user_achievement.rb
+++ app/models/user_achievement.rb
@@ -2,5 +2,5 @@ class UserAchievement < ApplicationRecord
   self.table_name = "gamedesk.user_achievements"
 
   belongs_to :achievement, class_name: "Achievement", foreign_key: :achievement_id
-  belongs_to :user, class_name: "User", foreign_key: :assignee_id, primary_key: :assignee_id
+  has_one :user, class_name: "User", foreign_key: :assignee_id, primary_key: :assignee_id
 end
