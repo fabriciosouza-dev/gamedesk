cf11e3380c56ddb3fc22ccc6ee5bf438de9510b6
Fabricio <fabricioeliasdesouza@gmail.com>
2021-08-26 23:28:10 -0300

    public update

--- db/migrate/20210826234900_rename_column_reward.rb
+++
@@ -1,8 +0,0 @@
-class RenameColumnReward < ActiveRecord::Migration[5.2]
-  def change
-
-    if table_exists? Reward.table_name
-      rename_column Reward.table_name, :descricao, :recompensa
-    end
-  end
-end
